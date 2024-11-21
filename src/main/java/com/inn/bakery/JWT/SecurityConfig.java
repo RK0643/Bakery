c6385c743082bd98e5848d05783431400a36c139
Mouli0910 <vinnakota.moulidhar9@gmail.com>
2024-11-21 08:26:01 +0530

    JWT updates 4th commit
    JWT updates

--- src/main/java/com/inn/bakery/JWT/SecurityConfig.java
+++ src/main/java/com/inn/bakery/JWT/SecurityConfig.java
@@ -1,11 +1,33 @@
 package com.inn.bakery.JWT;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig implements WebSecurityConfigurerAdapter{
+import static org.springframework.security.config.Customizer.withDefaults;
+
+public class SecurityConfig {
 
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(
+                        auth -> auth.requestMatchers("/signin", "/signup").permitAll()
+                                .requestMatchers("/users/**", "/apps/**").hasAuthority("ADMIN")
+                                .requestMatchers("/myapps/**").hasAuthority("CLIENT")
+                                .anyRequest().authenticated()
+                )
+                .formLogin(formLogin -> formLogin
+                        .loginPage("/signin")
+                        .usernameParameter("email")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()
+                )
+                .rememberMe(withDefaults())
+                .logout(logout -> logout.logoutUrl("/signout").permitAll());
+
+
+        return http.build();
+    }
 }
